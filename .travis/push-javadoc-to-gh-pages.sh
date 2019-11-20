a077fb50d196cabcae0269925a42a27751f856a0
Maxim Grischenko <grischenko.maxim@gmail.com>
2019-11-20 12:33:39 +0100

    determining right path

--- .travis/push-javadoc-to-gh-pages.sh
+++ .travis/push-javadoc-to-gh-pages.sh
@@ -10,8 +10,6 @@ then
 
   cp -R target/site/apidocs $HOME/javadoc-latest
 
-  ls target/site/apidocs
-
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
