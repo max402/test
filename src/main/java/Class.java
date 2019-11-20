43357a69950826bb6f6c41be63e495deb8a28f0d
Maxim Grischenko <grischenko.maxim@gmail.com>
2019-11-20 09:55:42 +0100

    test javadoc

--- src/main/java/Class.java
+++ src/main/java/Class.java
@@ -1,2 +1,17 @@
+import lombok.Data;
+
+/**
+ * This is class
+ */
+@Data
 public class Class {
+    String field1;
+    String field2;
+    /**
+     * method a
+     * @return string a
+     */
+    public String returnString() {
+        return "a";
+    }
 }
