eaeb8f2c494b994ca706d05668fdcf3b536c9c85
Maxim Grischenko <grischenko.maxim@gmail.com>
2019-11-22 15:58:03 +0100

    determining right path

--- .travis/push-javadoc-to-gh-pages.sh
+++ .travis/push-javadoc-to-gh-pages.sh
@@ -2,20 +2,20 @@
 
 if [[ "$TRAVIS_REPO_SLUG" == "max402/test"
     && "$TRAVIS_JDK_VERSION" == "openjdk8"
-    && "$TRAVIS_PULL_REQUEST" == "false" ]];
-#    &&  "$TRAVIS_TAG" ]];
+    && "$TRAVIS_PULL_REQUEST" == "false"
+    &&  "$TRAVIS_TAG" ]];
 then
 
   echo -e "Publishing javadoc...\n"
 
-  echo '$PWD is' $PWD # /home/travis/build/user/repo
-  echo '$HOME is' $HOME /home/travis
-  echo 'command pwd' pwd # ~/build/user/repo
+  # $PWD is /home/travis/build/user/repo
+  # $HOME is /home/travis
+  # pwd is ~/build/user/repo
 
   git clone --quiet --branch=gh-pages https://${GITHUB_TOKEN}@github.com/max402/test gh-pages > /dev/null
   cd gh-pages
   mkdir -p ./javadoc/$TRAVIS_TAG && cp -Rf ../target/site/apidocs/* ./javadoc/$TRAVIS_TAG
-#  ln -s ./javadoc/$TRAVIS_TAG ./javadoc/latest
+  ln -s $PWD/javadoc/$TRAVIS_TAG javadoc/latest
 
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
