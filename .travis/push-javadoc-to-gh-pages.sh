c003e9c80fc16d853d1fc2c99e4d1955323551e9
Maxim Grischenko <grischenko.maxim@gmail.com>
2019-11-22 16:37:42 +0100

    determining right path

--- .travis/push-javadoc-to-gh-pages.sh
+++ .travis/push-javadoc-to-gh-pages.sh
@@ -15,7 +15,7 @@ then
   git clone --quiet --branch=gh-pages https://${GITHUB_TOKEN}@github.com/max402/test gh-pages > /dev/null
   cd gh-pages
   mkdir -p ./javadoc/$TRAVIS_TAG && cp -Rf ../target/site/apidocs/* ./javadoc/$TRAVIS_TAG
-  ln -fs $PWD/javadoc/$TRAVIS_TAG javadoc/latest
+  ln -fs $TRAVIS_TAG javadoc/latest
 
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
